1a51d21c7f76c83547bb41b0b6ef81dfd301b60e
dil-lkobor <levente.kobor94@gmail.com>
2022-01-05 21:31:10 +0100

    add feedback widget

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express')
 
 const cors = require('cors')
-const logger = require("./logger")
 const routes = require('./routes');
 
 const port = process.env.PORT || 3000
@@ -12,6 +11,5 @@ app.use(cors())
 app.use('/', routes);
 
 app.listen(port, () => {
-    logger.info(process.env.BASE_URL)
-    logger.info(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,34 +3,60 @@ const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 const request = require('request')
 const routes = require('express').Router()
-const logger = require("../logger") 
-import { Low, JSONFile } from 'lowdb'
+const sqlite3 = require('sqlite3')
+const { Sequelize, DataTypes } = require('sequelize');
 
-const file = '../feedback.json'
-const adapter = new JSONFile(file)
-const db = new Low(adapter)
-await db.read()
+const sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './feedbacks.db',
+    define: {
+        timestamps: false
+    }
+})
 
-const { feedback } = db.data
-
+const Feedback = sequelize.define('Feedback', {
+    feedback: {
+      type: DataTypes.STRING,
+      allowNull: true
+    },
+    like: {
+      type: DataTypes.STRING
+    }
+  }, {
+})
+  
 const apicache = require('apicache')
 let cache = apicache.middleware
 
 const onlyStatus200 = (req, res) => res.statusCode === 200
 
 routes.get('/api/redmine_url', cache('5 minutes', onlyStatus200), async function(req, res) {
-    logger.info("Serving base URL")
     res.send(process.env.BASE_URL)
 })
 
 routes.post('/api/feedback', jsonParser, async function(req, res) {
-    const feedback = feedbacks.push(req.body)
-    await db.write()
-    res.send(feedback)
+    return Feedback.create({
+        feedback: req.body.feedback || '',
+        like: req.body.like,
+    }, { fields: ['feedback', 'like'] }).then(function (feedback) {
+        if (feedback) {
+            res.send(feedback);
+        } else {
+            res.status(500).send('Error in insert new record');
+        }
+    })  
 })
 
 routes.get('/api/feedback', jsonParser, async function(req, res) {
-
+    Feedback.findAll({ attributes: ['feedback', 'like']}).then(data => {
+        res.status(200).send(data)
+    })
+    .catch(err => {
+        res.status(500).send({
+            message:
+            err.message || "Some error occurred while retrieving tutorials."
+        })
+    })
 })
 
 routes.post('/api/login', jsonParser, async function(req, res) {
@@ -38,7 +64,6 @@ routes.post('/api/login', jsonParser, async function(req, res) {
 
     request(`https://${req.body.username}:${req.body.password}@${baseUrlDomain}users/current.json`, function (error, response, body) {
         if (!error && response.statusCode == 200) {
-            logger.info("User successfully logged in")
             res.send(response.body)
         } else {
             res.statusCode = 401;
@@ -49,15 +74,11 @@ routes.post('/api/login', jsonParser, async function(req, res) {
 
 routes.use('/api', cache('5 minutes', onlyStatus200), async function(req, res) {
     let startTime = new Date()
-    logger.info("Incoming " + req.method + " request")
-    logger.debug("request URL: " + req.url)
     req.pipe(request(process.env.BASE_URL + req.url)).pipe(res);
     let endTime = new Date()
-    logger.info("Response %s. Completed in: %dms", res.statusCode, (endTime - startTime))
 })
   
 routes.get('/cache/index', async function (req, res) {
-    logger.info("serving cache index")
     res.json(apicache.getIndex())
 })
 
